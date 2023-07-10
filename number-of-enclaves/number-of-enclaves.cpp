9534bae0fdf98a3c7273e481669544396cb141e4
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-07-10 20:24:39 +0530

    Time: 89 ms (39.25%) | Memory: 33.8 MB (19.59%) - LeetSync

--- number-of-enclaves/number-of-enclaves.cpp
+++ number-of-enclaves/number-of-enclaves.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    void dfs(int row, int col, vector<vector<int>> &grid, vector<vector<int>>&vis, int delrow[], int delcol[]){
+    void dfs(int row, int col, vector<vector<int>> &grid, vector<vector<int>>&vis, int delrow[], int delcol[] , int m , int n){
           vis[row][col] = 1;
           grid[row][col] = 0;
 
-          int m= grid.size();
-          int n= grid[0].size();
+      
 
           for(int i=0;i<4;i++){
               int nrow = row + delrow[i];
               int ncol = col + delcol[i];
 
               if(nrow >=0 && nrow < m && ncol >=0 && ncol <n && grid[nrow][ncol]==1 && !vis[nrow][ncol]){
-                  dfs(nrow, ncol , grid, vis, delrow, delcol);
+                  dfs(nrow, ncol , grid, vis, delrow, delcol , m , n);
               }
           }
     }
@@ -28,18 +27,18 @@ public:
         for(int i=0;i<m;i++){
            
            if(grid[i][0] == 1 && !vis[i][0]){
-              dfs(i , 0 , grid, vis, delrow, delcol);
+              dfs(i , 0 , grid, vis, delrow, delcol , m , n);
            }
            if(grid[i][n-1] == 1 && !vis[i][n-1]){
-               dfs(i , n-1, grid, vis, delrow, delcol);
+               dfs(i , n-1, grid, vis, delrow, delcol, m, n);
            }
         }
         for(int i=0;i<n;i++){
             if(grid[0][i] == 1 && !vis[0][i]){
-                dfs(0 , i , grid, vis, delrow, delcol);
+                dfs(0 , i , grid, vis, delrow, delcol, m, n);
             }
             if(grid[m-1][i] == 1 && !vis[m-1][i]){
-                dfs(m-1 , i, grid, vis , delrow , delcol);
+                dfs(m-1 , i, grid, vis , delrow , delcol , m, n);
             }
         }
         int count=0;
