b84ac21a40f54b777e820ca23c317c74100a050b
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-07-12 09:08:22 +0530

    Time: 3 ms (68.10%) | Memory: 7.6 MB (60.67%) - LeetSync

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-   void solve(vector<vector<int>>&ans, vector<int>&temp, vector<int>&nums, int idx, int n , unordered_map<int,bool>&mp){
-          if(idx == n){
-              ans.push_back(temp);
-              return;
-          }
-
-          for(int i=0 ;i<n;i++){
-              if(!mp[nums[i]]){
-                  mp[nums[i]]=true;
-                  temp.push_back(nums[i]);
-                  solve(ans, temp, nums,idx+1, n , mp);
-                  temp.pop_back();
-                  mp[nums[i]]=false;
-              }
-          }
-   }
+    void solve(vector<vector<int>>&ans, vector<int>&nums, int idx, int n){
+        if(idx == n){
+            ans.push_back(nums);
+            return ;
+        }
+        for(int i= idx ;i<n ;i++){
+            swap(nums[i], nums[idx]);
+            solve(ans, nums, idx+1 , n);
+            swap(nums[idx] , nums[i]);
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        unordered_map<int, bool>mp;
-        vector<int>temp;
-       solve(ans,temp, nums, 0 , nums.size(), mp);
-       return ans;
+        solve(ans, nums, 0, nums.size());
+        return ans;
     }
 };
