38195579cc42558936580e5dfb90156d4f98997f
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-07-13 07:42:06 +0530

    Time: 0 ms (100.00%) | Memory: 6.1 MB (36.47%) - LeetSync

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int solve(int n , vector<int>&dp){
+        if(n<=0) return 1;
+        if(n==1) return 1;
+
+        if(dp[n] != -1) return dp[n];
+
+        int left =  solve(n-1, dp);
+        int right=0;
+        if(n>1) right = solve(n-2, dp);
+
+        return dp[n] =left+right;
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1 , -1);
+        return solve(n,dp);
+    }
+};
