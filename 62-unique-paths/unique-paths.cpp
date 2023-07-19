07cfdab020e3212f0dad7cdbbf40283802d7f0ef
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-07-19 10:05:30 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (20.32%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -17,7 +17,22 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m, vector<int>(n, -1));
-        return solve(m-1 , n-1 , dp);
-        
+       
+       dp[0][0]=1;
+
+       for(int i=0;i<m;i++){
+           for(int j=0;j<n;j++){
+               if(i==0 && j==0) continue;
+
+               int left =0;
+               if(j>0) left += dp[i][j-1];
+
+               int up=0;
+               if(i>0) up+= dp[i-1][j];
+
+               dp[i][j] = left+ up;
+           }
+       }
+        return dp[m-1][n-1];
     }
 };
