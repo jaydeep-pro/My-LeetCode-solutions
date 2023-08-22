3a871083f8c575d4bc56d0fd3943758d0a6198c1
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-08-22 23:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 6 MB (37.93%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        vector<char>v;
-        for(char i = 'A';i<='Z';i++){
-            v.push_back(i);
-        }
-      
       string ans = "";
       while(columnNumber>0){
           columnNumber--;
           int remainder = columnNumber % 26;
           columnNumber /= 26;
-          ans = v[remainder] + ans;
+          ans = (char)(remainder + 'A') + ans;
       }
     
       return ans;
