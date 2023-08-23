7080d39cb459670ea14d631d15b1d306afd3ccf7
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-08-23 16:31:39 +0530

    Time: 0 ms (100.00%) | Memory: 6.5 MB (10.98%) - LeetSync

---
+++ 778-reorganize-string/reorganize-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string reorganizeString(string s) {
+       string ans;
+       priority_queue<pair<int, char>>pq;
+      int arr[26]= {0};
+
+       for(char c : s){
+           arr[c - 'a']++;
+       }
+       for(int i=0;i<26;i++){
+           if(arr[i] >0){
+               pq.push({arr[i], i + 'a' });
+           }
+       }
+
+       while(!pq.empty()){
+          char first = pq.top().second;
+          int count1 = pq.top().first;
+          pq.pop();
+         
+
+         if(ans.size() == 0 || ans.back() != first){
+             ans += first;
+
+             if(--count1 >0) pq.push({count1 , first});
+         }
+         else{
+             if(pq.size() ==0) return "";
+         
+             char second = pq.top().second;
+             int count2 = pq.top().first;
+
+             pq.pop();
+
+             ans += second;
+             if(--count2 >0) pq.push({ count2 , second});
+
+            pq.push({count1 , first});
+       }
+       }
+       return ans;
+    }
+};
