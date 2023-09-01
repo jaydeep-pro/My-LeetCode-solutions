5ddb6eccdf1a0110c555000f24a22297136b3ab6
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-09-01 11:00:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (14.67%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -3,8 +3,12 @@ public:
     vector<int> countBits(int n) {
         vector<int>ans;
         for(int i=0;i<=n;i++){
-            int count =__builtin_popcount(i);
-           
+            int count =0;
+            int nums=i;
+            while(nums){
+               count += nums%2;
+               nums = nums/2;
+            }
             ans.push_back(count);
         }
         return ans;
