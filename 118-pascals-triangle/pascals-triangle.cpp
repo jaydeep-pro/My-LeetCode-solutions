fbbfee8f0b496f82804aac844520be8f57a900a6
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-09-08 16:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (49.34%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -5,10 +5,8 @@ public:
         ans.push_back({1});
         if(numRows == 1) return ans;
 
-        ans.push_back({1,1});
-        if(numRows == 2) return ans;
-
-        for(int i=2;i<numRows;i++){
+       
+        for(int i=1;i<numRows;i++){
             vector<int>temp(i+1, 1);
             for(int j=1;j<i;j++){
                 temp[j] = ans[i-1][j-1]+ ans[i-1][j];
