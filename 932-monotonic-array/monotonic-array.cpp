49de5c467c1d47dbe176daf2c83e8ba9257a872a
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-09-29 19:42:19 +0530

    Time: 138 ms (12.66%) | Memory: 96.9 MB (13.85%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    bool dec(vector<int>& nums){
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] < nums[i+1]) return false;
-        }
-        return true;
-    }
-   
     bool isMonotonic(vector<int>& nums) {
         bool increase = true, decrease= true;
         for(int i=0;i<nums.size()-1;i++){
