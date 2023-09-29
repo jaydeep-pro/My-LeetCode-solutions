00502d57389d99fe07f54e4d77cd740e3c652ffb
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-09-29 19:41:17 +0530

    Time: 111 ms (90.42%) | Memory: 96.7 MB (22.05%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -6,13 +6,15 @@ public:
         }
         return true;
     }
-    bool inc(vector<int>& nums){
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i] > nums[i+1]) return false;
-        }
-        return true;
-    }
+   
     bool isMonotonic(vector<int>& nums) {
-        return (dec(nums) || inc(nums));
+        bool increase = true, decrease= true;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i] < nums[i+1]) decrease = false;
+            if(nums[i] > nums[i+1]) increase = false;
+
+        }
+        return increase || decrease;
+    
     }
 };
