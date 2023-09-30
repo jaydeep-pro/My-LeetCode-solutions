909212892775f52525d7a0020931af3061e82baf
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-09-30 22:09:09 +0530

    Time: 68 ms (58.88%) | Memory: 48.9 MB (32.15%) - LeetSync

---
+++ 456-132-pattern/132-pattern.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        stack<int>st;
+        int prev = INT_MIN;
+
+        for(auto it = nums.rbegin(); it != nums.rend(); it++){
+            while(!st.empty() && *it > st.top()){
+                if(prev > st.top()) return true;
+
+                prev = st.top();
+                st.pop();
+            }
+            st.push(*it);
+        }
+      return !st.empty() && prev > st.top();
+    }
+};
