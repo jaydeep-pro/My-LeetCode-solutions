412195370d4fc3914c66e9bf37ba19e3aec989b0
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-10-21 19:49:38 +0530

    Time: 5 ms (16.41%) | Memory: 6.6 MB (9.70%) - LeetSync

--- 258-add-digits/add-digits.cpp
+++ 258-add-digits/add-digits.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int addDigits(int num) {
-        int sum=0;
-
-        while(true){
-           while(num){
-             sum += num%10;
-            num = num/10;
-           }
-
-            if(sum <= 9) return sum;
-
-            num = sum;
-            sum =0;
-        }
-        return 0;
+        if(num ==0 ) return 0;
+        return  num%9 == 0 ? 9 : num%9;
     }
 };
