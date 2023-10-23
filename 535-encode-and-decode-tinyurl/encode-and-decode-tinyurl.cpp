27b65d85e2c1cf1f766538e436d9e8ba64175e4f
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-10-23 14:22:59 +0530

    Time: 4 ms (36.44%) | Memory: 8 MB (5.54%) - LeetSync

---
+++ 535-encode-and-decode-tinyurl/encode-and-decode-tinyurl.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    unordered_map<string, string> long_to_short;
+    unordered_map<string, string> short_to_long;
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+ long_to_short[longUrl] = "http://jaydeep.com/" + to_string(hash_function(longUrl));
+ short_to_long[long_to_short[longUrl]] = longUrl;
+      return long_to_short[longUrl];
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+       return short_to_long[shortUrl];
+    }
+
+    private:
+    uint64_t hash_function( string& str) {
+        uint64_t hash = 9239;
+      uint64_t prime = 9999;
+
+        for(int i = 0; i < str.size(); ++i) {
+           uint64_t value = str[i];
+            hash = hash ^ value;
+            hash *= prime;
+        }
+
+        return hash;
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
