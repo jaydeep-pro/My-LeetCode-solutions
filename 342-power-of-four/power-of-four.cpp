c8f68c6918d5072cb5149d7634a5c80de55d694c
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-10-23 10:41:54 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (47.70%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 0) return false;
-        double x = (log(n))/log(4);
+        // if(n == 0) return false;
+        // double x = (log(n))/log(4);
 
-         if(trunc(x) == x) return true;
+        //  if(trunc(x) == x) return true;
 
-        // if(floor(x) == x) return true;
-        return false;
+        // // if(floor(x) == x) return true;
+        // return false;
+
+        return __builtin_popcount(n) == 1 && __builtin_ctz(n)%2 ==0;
 
 
     }
