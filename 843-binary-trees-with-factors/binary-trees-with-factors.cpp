9409e6c14342caef4a5ab73f11e6236f62998904
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-10-26 12:11:17 +0530

    Time: 71 ms (27.17%) | Memory: 16.9 MB (16.54%) - LeetSync

--- 843-binary-trees-with-factors/binary-trees-with-factors.cpp
+++ 843-binary-trees-with-factors/binary-trees-with-factors.cpp
@@ -5,17 +5,16 @@ public:
         long ans =0;
         sort(arr.begin(), arr.end());
         unordered_map<int,long>mp;
-         for(int i=0;i<arr.size();i++) mp[arr[i]]++;
        
         for(int i=0;i<arr.size();i++){
              long temp = 1;
 
              for(int j=0;j<i;j++){
-                 if(arr[i]%arr[j]) continue;
+                 if(arr[i]%arr[j] ==0){
                  int a = arr[i]/arr[j];
                  int b = arr[j];
                  temp = temp + (mp[a]*mp[b])%mod;
-             }
+             }}
              mp[arr[i]] = temp;
              ans += temp;
         }
