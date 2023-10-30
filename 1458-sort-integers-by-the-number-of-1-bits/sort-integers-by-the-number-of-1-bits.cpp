71d2a717b6f94ace5a15aaa57330ee385fe673ed
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-10-30 22:30:26 +0530

    Time: 8 ms (53.22%) | Memory: 10.9 MB (35.82%) - LeetSync

---
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int noofbits(int n){
+        int bits =0;
+        while(n){
+            bits += (n%2);
+            n = n/2;
+        }
+        return bits;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+         for(int i=0;i<arr.size();i++){
+             pq.push({noofbits(arr[i]), arr[i]});
+         }
+
+         arr.clear();
+         while(!pq.empty()){
+             arr.push_back(pq.top().second);
+             pq.pop();
+         }
+         return arr;
+    }
+};
