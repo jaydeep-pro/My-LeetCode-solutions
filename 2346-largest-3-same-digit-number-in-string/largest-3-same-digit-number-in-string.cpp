ad4b6a2a9a9f6e0dc79306bbfecf6ac9b6fa2dbe
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-12-04 19:53:01 +0530

    Time: 4 ms (47.83%) | Memory: 6.8 MB (59.71%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -6,14 +6,7 @@ public:
         for(int i=0;i<num.size()-2;i++){
           if(num[i] == num[i+1] && num[i+1] == num[i+2]){
               temp = num.substr(i,3);
-             int a= stoi(temp);
-             if(ans != ""){ int b = stoi(ans);
-              if(a>b) ans= temp;
-             }
-
-             else{
-                  ans = temp;
-              }
+            if(temp > ans) ans = temp;
           } 
         }
         return ans;
