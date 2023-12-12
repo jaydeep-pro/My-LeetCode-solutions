ccf13261d2d6fc07842c08679c3c8ab5bcea8ed8
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-12-12 14:27:43 +0530

    Time: 3 ms (82.74%) | Memory: 10.3 MB (62.52%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -5,16 +5,13 @@ public:
        int second_maxi = INT_MIN;
 
        for(int i=0;i<nums.size();i++){
-           first_maxi = max(first_maxi, nums[i]);
-       }
-       for(int i=0;i<nums.size();i++){
-           if(nums[i] == first_maxi){
-               nums[i] = INT_MIN;
-               break;
+           if(nums[i] > first_maxi){
+               second_maxi = first_maxi;
+               first_maxi = nums[i];
            }
-       }
-        for(int i=0;i<nums.size();i++){
-           second_maxi = max(second_maxi, nums[i]);
+           else{
+               second_maxi = max(second_maxi , nums[i]);
+           }
        }
        return (first_maxi -1 ) * (second_maxi -1);
     }
