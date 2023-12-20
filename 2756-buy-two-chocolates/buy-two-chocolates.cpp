a920dbbfbf67812ebbfa5fc9685eb186146ecf9a
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-12-20 11:14:18 +0530

    Time: 14 ms (48.64%) | Memory: 47.2 MB (13.41%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end());
-
-        if(prices[0] + prices[1] <= money){
-            return ((money) - (prices[0] + prices[1]));
+        int firstmin = min(prices[0], prices[1]);
+        int secondmin = max(prices[0], prices[1]);
+        
+        for(int i=2;i<prices.size();i++){
+            if(prices[i] < firstmin){
+                secondmin = firstmin;
+                firstmin = prices[i];
+            }else if(prices[i] < secondmin){
+                secondmin = prices[i];
+            }
+    
         }
+        if(firstmin + secondmin <= money) return money - firstmin - secondmin;
         return money;
+
     }
 };
